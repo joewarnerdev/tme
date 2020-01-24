e8add96afad1d2d8c7ee98435597e6b1e3092dd3
Joe Warner <joewarnerdev@gmail.com>
2020-01-23 19:35:23 -0600

    adding features
    added beforeEach feature

--- sampleproject/test/forEach.test.js
+++ sampleproject/test/forEach.test.js
@@ -1,8 +1,13 @@
 const assert = require('assert');
 const { forEach } = require('../index');
 
+let numbers;
+
+beforeEach(() => {
+    numbers = [1,2,3];
+});
+
 it('Should sum an array', () => {
-    const numbers = [1,2,3];
 
     let total = 0;
     forEach(numbers, value => {
@@ -10,4 +15,9 @@ it('Should sum an array', () => {
     });
 
     assert.strictEqual(total, 6);
+});
+
+it('beforeEach is ran each time', () => {
+    assert.strictEqual(numbers.length, 3);
+    
 });
